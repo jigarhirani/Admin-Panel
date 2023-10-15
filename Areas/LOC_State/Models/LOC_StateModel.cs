01fe039e4cf8a254fbb853486cb980170e14a648
Jigar <hirani.jigar117@gmail.com>
2023-10-15 11:43:07 +0530

    Directory Changes

---
+++ Areas/LOC_State/Models/LOC_StateModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admin_Panel.Areas.LOC_State.Models
+{
+    public class LOC_StateModel
+    {
+        public int? StateID { get; set; }
+
+        [Required(ErrorMessage = "Please Enter State Name")]
+        public string? StateName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter State Code")]
+        public string? StateCode { get; set; }
+
+        [Required(ErrorMessage = "Please Country Select")]
+        public int? CountryID { get; set; }
+    }
+
+    public class LOC_StateDropDownModel
+    {
+
+        public int StateID { get; set; }
+        public String? StateName { get; set; }
+
+    }
+}
